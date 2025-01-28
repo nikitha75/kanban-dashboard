4eb1a88420cbbc429645d48575faafe4e782b868
Nikitha75 <nikithanikki474@gmail.com>
2025-01-29 03:14:46 +0530

    Add users data

---
+++ src/utils/data/userData.js
@@ -0,0 +1,62 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const users = [
+  {
+    userId: uuidv4(),
+    userName: "Aastha Agarwal",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Ishaan Sharma",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Riaan Mehta",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Samaira Mishra",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Vihaan Patel",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Zara Sharma",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Yash Sen",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Riya Patel",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Nisha Mehta",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Rohit Roy",
+  },
+  {
+    userId: uuidv4(),
+    userName: "Alina Bansal",
+  },
+];
+
+export const uColors = [
+  { bgColor: "#9030cc", color: "#FFFFFF" }, // Purple background, white text
+  { bgColor: "#F9A8D4", color: "#000000" }, // Light pink background, black text
+  { bgColor: "#BAE6FD", color: "#000000" }, // Light sky background, black text
+  { bgColor: "#86EFAC", color: "#000000" }, // Light green background, black text
+  { bgColor: "#D8B4FE", color: "#000000" }, // Light purple background, black text
+  { bgColor: "#FCD34D", color: "#000000" }, // Light yellow background, black text
+  { bgColor: "#1D4ED8", color: "#FFFFFF" }, // Dark blue background, white text
+  { bgColor: "#A3E635", color: "#000000" }, // Lime green background, black text
+  { bgColor: "#86198F", color: "#FFFFFF" }, // Dark fuchsia background, white text
+  { bgColor: "#2DD4BF", color: "#FFFFFF" }, // Teal background, white text
+  { bgColor: "#D4D4D8", color: "#000000" }, // Zinc background, black text
+];
